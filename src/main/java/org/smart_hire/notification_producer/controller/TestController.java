fdf5bfdaef224ac8cdaf15ef17bd98b8d5099c24
jisu2000 <spjisu2000@gmail.com>
2025-06-18 19:18:40 +0530

    Test Controller added

--- src/main/java/org/smart_hire/notification_producer/controller/TestController.java
+++ src/main/java/org/smart_hire/notification_producer/controller/TestController.java
@@ -1,11 +1,14 @@
 package org.smart_hire.notification_producer.controller;
 
 import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
     
+    @GetMapping("/test")
     public Map<String,String> test(){
         return Map.of("msg","Kafka producer is up");
     }
