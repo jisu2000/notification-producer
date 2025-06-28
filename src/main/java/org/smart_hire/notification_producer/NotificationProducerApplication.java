83cc3288fa8ed71be6f6c099c805f3ba2a52f861
Subhadeep Pattanayak <103503130+jisu2000@users.noreply.github.com>
2025-06-28 22:10:51 +0530

    Eureka set up

--- src/main/java/org/smart_hire/notification_producer/NotificationProducerApplication.java
+++ src/main/java/org/smart_hire/notification_producer/NotificationProducerApplication.java
@@ -2,8 +2,10 @@ package org.smart_hire.notification_producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NotificationProducerApplication {
 
 	public static void main(String[] args) {
